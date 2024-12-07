f0f24e74e553525e4d5aa62455b022d3f45b54ce
Nazar Chopyk <nazar.chopyk@gmail.com>
2024-12-07 09:51:35 +0200

    Updated topology.js

--- src/topology.js
+++ src/topology.js
@@ -1,40 +1,15 @@
 const topology = {
   'ip': '192.168.0.104',
   'port': 8002,
-  'rating': 0.3345,
+  'rating': 0.3250,
   'connectedClients': [
     {
       'ip': '192.168.0.105',
       'port': 8003,
-      'rating': 0.3951,
+      'rating': 0.362,
       'connectedClients': [
-        {
-          'ip': '192.168.0.107',
-          'port': null,
-          'rating': null,
-          'connectedClients': null
-        },
-        {
-          'ip': '192.168.0.108',
-          'port': null,
-          'rating': null,
-          'connectedClients': null
-        },
         {
           'ip': '192.168.0.109',
-          'port': null,
-          'rating': null,
-          'connectedClients': null
-        }
-      ]
-    },
-    {
-      'ip': '192.168.0.106',
-      'port': 8004,
-      'rating': 0.3875,
-      'connectedClients': [
-        {
-          'ip': '192.168.0.110',
           'port': null,
           'rating': null,
           'connectedClients': null
@@ -54,12 +29,37 @@ const topology = {
       ]
     },
     {
-      'ip': '192.168.0.107',
-      'port': 8005,
-      'rating': 0.5621,
+      'ip': '192.168.0.106',
+      'port': 8004,
+      'rating': 0.341,
       'connectedClients': [
         {
-          'ip': '192.168.0.113',
+          'ip': '192.168.0.118',
+          'port': null,
+          'rating': null,
+          'connectedClients': null
+        },
+        {
+          'ip': '192.168.0.121',
+          'port': null,
+          'rating': null,
+          'connectedClients': null
+        },
+        {
+          'ip': '192.168.0.122',
+          'port': null,
+          'rating': null,
+          'connectedClients': null
+        }
+      ]
+    },
+    {
+      'ip': '192.168.0.107',
+      'port': 8005,
+      'rating': 0.508,
+      'connectedClients': [
+        {
+          'ip': '192.168.0.123',
           'port': null,
           'rating': null,
           'connectedClients': null
